aa17b4f2556845be0d5a96bce2237b181d0fe048
Oğuzhan Arar <ichbinizm@gmail.com>
2020-04-02 18:52:08 +0300

    Third level is changed

--- Oguzhan_Arar_CarGame/Assets/Scripts/CarModelController.cs
+++ Oguzhan_Arar_CarGame/Assets/Scripts/CarModelController.cs
@@ -6,15 +6,16 @@ public class CarModelController : MonoBehaviour
 {
     public Color Car_Color;
     [SerializeField]
-    private GameObject[] cars;
-    [SerializeField]
     private int R = 128,G = 128,B = 0;
+    private GameObject menucar;
     void Update (){
       if(Application.loadedLevel == 0){
-        GameObject.Find("Menu_Car").gameObject.GetComponent<Renderer>().material.color = Car_Color;
+        menucar = GameObject.Find("Menu_Car").gameObject;
+        menucar.gameObject.GetComponent<Renderer>().material.color = Car_Color;
       }
        Car_Color = new Color(R/255f,G/255f,B/255f);
     }
+
     public void ColorChange(){
         R = Random.Range(0, 255);
         G = Random.Range(0, 255);
